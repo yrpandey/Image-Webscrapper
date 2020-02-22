110352876a0ff757381069d9d1752ba85acb6d03
yrpandey <57836647+yrpandey@users.noreply.github.com>
2020-02-22 22:10:00 +0530

    Add files via upload

---
+++ businesslayer/BusinessLayerUtil.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 22 21:01:40 2020
+
+@author: Yograj
+"""
+
+from scrapperImage.ScrapperImage import ScrapperImage
+
+class BusinessLayer:
+    
+    keyword=""
+    fileLoc=""
+    image_name=""
+    header=""
+     
+    def downloadImages( keyWord, header):
+        imgScrapper = ScrapperImage
+        url = imgScrapper.createImageUrl(keyWord)
+        rawHtml = imgScrapper.scrap_html_data(url, header)
+        
+        imageURLList = imgScrapper.getimageUrlList(rawHtml)
+        
+        masterListOfImages = imgScrapper.downloadImagesFromURL(imageURLList,keyWord, header)
+        
+        return masterListOfImages    
+   
+    
+    
